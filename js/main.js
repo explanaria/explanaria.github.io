22125e3157de7975a2a898a6f3c571499cd69631
Explanaria <explanaria@gmail.com>
2018-02-10 23:07:17 -0500

    From now on, this will be known as explanaria!

--- js/main.js
+++ js/main.js
@@ -31,3 +31,44 @@ new BlendAnimation({
 	});
 
 and then have this register somewhere, and then three.on('update'), advance all animations.
+
+new BlendAnimation({
+	from: {(i,t,x,y) => [x,y]}
+	to: {expr:  (i,t,x,y) => [x /(x*x+y*y), (-y) /(x*x+y*y)] }
+	});
+
+/* Manim solves this problem by saying "you construct all your transformations ahead of time, and then it all animates when you call wait(2). In other words, they're
+
+def test(){
+	animate(x,
+		{from:{expr:()=>[1,1]},
+		{to:{expr:(i,t,x)=>[x,0]}})
+	wait();
+
+	continuousAnimation(x,
+		{expr:(i,t,x)=>[x,1]},
+		{expr:(i,t,x)=>[x,2]}, 1)
+
+	wait(5)
+}
+
+	Here, test() goes in real time as it executes. wait() is blocking.
+
+I could replicate that with javascript's await:
+
+def test(){
+	animate(x,
+		{from:{expr:()=>[1,1]},
+		{to:{expr:(i,t,x)=>[x,0]}})
+	await three.nextSlide();
+
+	continuousAnimation(x,
+		{expr:(i,t,x)=>[x,1]},
+		{expr:(i,t,x)=>[x,2]}, 1)
+
+	await three.nextSlide();
+}
+
+Here, I'm using 'slide' as "a powerpoint slide". Multiple keystrokes might be in one slide, so multiple slides might be in one scene.
+
+Another way to do it is to

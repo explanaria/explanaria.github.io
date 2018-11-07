ca757fc910194f3596e22d31273e9b01b5c3f217
Explanaria <explanaria@gmail.com>
2018-11-07 10:47:48 -0500

    Make gridlines look better

--- src/js/outputs/SurfaceOutputShaders.js
+++ src/js/outputs/SurfaceOutputShaders.js
@@ -55,14 +55,42 @@ var fShader = [
 "   return vec4(diffuse*color + 0.9*rimLighting*color + 0.4*color2 * specular, rgba.a);",
 "}",
 
-"vec4 gridLineColor(vec2 uv, vec4 color) {",
+// Smooth HSV to RGB conversion from https://www.shadertoy.com/view/MsS3Wc
+"vec3 hsv2rgb_smooth( in vec3 c ){",
+"    vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );",
+"	rgb = rgb*rgb*(3.0-2.0*rgb); // cubic smoothing	",
+"	return c.z * mix( vec3(1.0), rgb, c.y);",
+"}",
+
+//From Sam Hocevar: http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
+"vec3 rgb2hsv(vec3 c){",
+"    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);",
+"    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));",
+"    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));",
+
+"    float d = q.x - min(q.w, q.y);",
+"    float e = 1.0e-10;",
+"    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);",
+"}",
+ //chooses the color for the gridlines by varying lightness. 
+//NOT continuous or else by the intermediate function theorem there'd be a point where the gridlines were the same color as the material.
+"vec3 gridLineColor(vec3 color){",
+" vec3 hsv = rgb2hsv(color.xyz);",
+" //hsv.x += 0.1;",
+" if(hsv.z < 0.8){hsv.z += 0.2;}else{hsv.z = 0.85-0.1*hsv.z;hsv.y -= 0.0;}",
+" return hsv2rgb_smooth(hsv);",
+"}",
+
+"vec4 renderGridlines(vec4 mainColor, vec2 uv, vec4 color) {",
 "  vec2 distToEdge = abs(mod(vUv.xy*gridSquares + lineWidth/2.0,1.0));",
+"  vec3 gridColor = gridLineColor(color.xyz);",
+
 "  if( distToEdge.x < lineWidth){",
-"    return vec4(offSpecular(color.xyz), color.a);",
+"    return showGrid*vec4(gridColor, color.a) + (1.-showGrid)*mainColor;",
 "  } else if(distToEdge.y < lineWidth){ ",
-"    return vec4(offSpecular(color.xyz), color.a);",
+"    return showGrid*vec4(gridColor, color.a) + (1.-showGrid)*mainColor;",
 "  }",
-"  return vec4(0.0);",
+"  return mainColor;",
 "}",
 /*
 "vec4 getShadedColorMathbox(vec4 rgba) { ",
@@ -85,9 +113,9 @@ var fShader = [
 //"  //gl_FragColor = vec4(vNormal.xyz, 1.0); // view debug normals",
 //"  //if(vNormal.x < 0.0){gl_FragColor = vec4(offSpecular(color.rgb), 1.0);}else{gl_FragColor = vec4((color.rgb), 1.0);}", //view specular and non-specular colors
 //"  gl_FragColor = vec4(mod(vUv.xy,1.0),0.0,1.0); //show uvs
-"  vec4 materialColor = getShadedColor(vec4(color.rgb, 1.0));",
-"  vec4 gridLineColor = gridLineColor(vUv.xy, vec4(color.rgb, 1.0));",
-"  gl_FragColor = showSolid*materialColor + showGrid*gridLineColor;",	
+"  vec4 materialColor = showSolid*getShadedColor(vec4(color.rgb, 1.0));",
+"  vec4 colorWithGridlines = renderGridlines(materialColor, vUv.xy, vec4(color.rgb, 1.0));",
+"  gl_FragColor = colorWithGridlines;",	
 "}"].join("\n")
 
 var uniforms = {

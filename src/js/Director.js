4268d88dd2814de51a55b6e5e931f78617fb1881
Explanaria <explanaria@gmail.com>
2019-05-03 14:30:31 -0400

    Begin work on an UndoCapableDirector

--- src/js/Director.js
+++ src/js/Director.js
@@ -155,10 +155,10 @@ class NonDecreasingDirector{
 		//right now there is a problem. Going backwards should not resolve the promise; only going to the most recent slide and pressing right should.
 		if(slideDelta != 0){
 			if(this.currentSlideIndex == 0 && slideDelta == -1){
-				return;
+				return; //no going past the beginning
 			}
 			if(this.currentSlideIndex == this.slides.length-1 && slideDelta == 1){
-				return;
+				return; //no going past the end
 			}
 			this.currentSlideIndex += slideDelta;
 			this.showSlide(this.currentSlideIndex);
@@ -176,73 +176,50 @@ class NonDecreasingDirector{
 		new Animation(target, toValues, durationMS === undefined ? undefined : durationMS/1000);
 	}
 }
-/*
-class Director{
+
+class UndoCapableDirector extends NonDecreasingDirector{
 	//todo. Make this able to backtrack
 	constructor(options){
-		this.undoStack = [];
-		this.undoStackIndex = 0;
-
-		this.slides = document.getElementsByClassName("exp-slide");
-		this.currentSlideIndex = 0;
-		//this.showSlide(0); //fails because DOM isn't loaded.
+        super(options);
 	}
 
-	async waitForPageLoad(){
-		return new Promise(function(resolve, reject){
-			window.addEventListener("load",resolve);
-		});
-	}
+	async nextSlide(){    if(!this.initialized)throw new Error("ERROR: Use .begin() on a Director before calling any other methods!");
 
-	showSlide(slideNumber){
-		for(var i=0;i<this.slides.length;i++){
-			this.slides[i].style.opacity = 0;
-		}
-		this.slides[slideNumber].style.opacity = 1;
-	}
-
-	nextSlide(){
 		let self = this;
+
+		this.rightArrow.showSelf();
+		//promise is resolved by calling this.nextSlidePromise.resolve() when the time comes
+
 		return new Promise(function(resolve, reject){
-			window.addEventListener("keypress", function keyListener(e){
+			function keyListener(e){
+				if(e.repeat)return; //keydown fires multiple times but we only want the first one
 				let slideDelta = 0;
 				switch (e.keyCode) {
-				  case 33:
-				  case 37:
-				  case 38:
-					slideDelta = -1;
-					break;
 				  case 34:
 				  case 39:
 				  case 40:
 					slideDelta = 1;
 					break;
+                  case 33:
+                  case 37:
+                  case 38:
+                    slideDelta = -1;
+				  default:
+					break;
 				}
-				self._changeSlide(slideDelta, resolve);
-				window.removeEventListener("keypress",keyListener); //this approach taken from https://stackoverflow.com/questions/35718645/resolving-a-promise-with-eventlistener
-			});
-		});
-	}
-	_changeSlide(slideDelta, resolve){
-			//slide changing logic
-		if(slideDelta != 0){
-			if(this.currentSlideIndex == 0 && slideDelta == -1){
-				return;
+				if(slideDelta != 0){
+					self._changeSlide(slideDelta, resolve);
+					self.rightArrow.hideSelf();
+					window.removeEventListener("keydown",keyListener); //this approach taken from https://stackoverflow.com/questions/35718645/resolving-a-promise-with-eventlistener
+				}
 			}
-			if(this.currentSlideIndex == this.slides.length-1 && slideDelta == 1){
-				return;
-			}
-				console.log(slideDelta, this.currentSlideIndex);
-			this.currentSlideIndex += slideDelta;
-			this.showSlide(this.currentSlideIndex);
-			resolve();
-		}
-	}
 
-	//verbs
-	async delay(waitTime){
-		return new Promise(function(resolve, reject){
-			window.setTimeout(resolve, waitTime);
+			window.addEventListener("keydown", keyListener);
+			//horrible hack so that the 'next slide' arrow can trigger this too
+			self.nextSlideResolveFunction = function(){ 
+				resolve();
+				window.removeEventListener("keydown",keyListener); 
+			}
 		});
 	}
 	transitionTo(target, toValues, durationMS){
@@ -261,6 +238,11 @@ EXP.Director.UndoItem = class UndoItem{
 		this.fromValues = fromValues;
 		this.durationMS = durationMS;
 	}
-}*/
+}
+EXP.Director.NewSlideUndoPoint = class NewSlide{
+	constructor(slideIndex){
+        this.slideIndex = slideIndex;
+	}
+}
 
-export { NonDecreasingDirector, DirectionArrow };
+export { NonDecreasingDirector, DirectionArrow, UndoCapableDirector };

7f9d14c582c8e44d90cc66634796ba0a405cdcd1
Explanaria <explanaria@gmail.com>
2019-05-03 17:19:14 -0400

    Implement UndoCapableDirector! Clickable arrows do nothing yet though.

--- src/js/Director.js
+++ src/js/Director.js
@@ -54,11 +54,10 @@ class DirectionArrow{
 class NonDecreasingDirector{
 	// I want Director() to be able to backtrack by pressing backwards. This doesn't do that.
 	constructor(options){
-		this.undoStack = [];
-		this.undoStackIndex = 0;
-
 		this.slides = [];
-		this.currentSlideIndex = 0;
+		this.currentSlideIndex = 0;        
+        this.numSlides = 0;
+        this.numHTMLSlides = 0;
 
 		this.nextSlideResolveFunction = null;
         this.initialized = false;
@@ -68,9 +67,10 @@ class NonDecreasingDirector{
 	async begin(){
 		await this.waitForPageLoad();
         this.slides = document.getElementsByClassName("exp-slide");
+        this.numHTMLSlides = this.slides.length;
 
         //hide all slides except first one
-		for(var i=0;i<this.slides.length;i++){
+		for(var i=0;i<this.numHTMLSlides;i++){
             this.slides[i].style.opacity = 0; 
 			this.slides[i].style.display = 'none';//opacity=0 alone won't be instant because of the 1s CSS transition
 		}
@@ -106,9 +106,13 @@ class NonDecreasingDirector{
 	}
 
 	showSlide(slideNumber){
-		for(var i=0;i<this.slides.length;i++){
+		for(var i=0;i<this.numHTMLSlides;i++){
 			if(i != slideNumber)this.slides[i].style.opacity = 0;
 		}
+        if(slideNumber >= this.numHTMLSlides){
+            console.error("Tried to show slide #"+slideNumber+", but only " + this.numHTMLSlides + "HTML elements with exp-slide were found! Make more slides?");
+            return;
+        }
 		this.slides[slideNumber].style.opacity = 1;
 	}
 
@@ -149,9 +153,7 @@ class NonDecreasingDirector{
 		});
 	}
 	_changeSlide(slideDelta, resolve){
-			//slide changing logic
-
-
+		//slide changing logic
 		//right now there is a problem. Going backwards should not resolve the promise; only going to the most recent slide and pressing right should.
 		if(slideDelta != 0){
 			if(this.currentSlideIndex == 0 && slideDelta == -1){
@@ -172,66 +174,168 @@ class NonDecreasingDirector{
 		});
 	}
 	TransitionTo(target, toValues, durationMS){
-		//EXP.Utils.Assert(this.undoStackIndex == 0); //This may not work well.
+		//Utils.Assert(this.undoStackIndex == 0); //This may not work well.
 		new Animation(target, toValues, durationMS === undefined ? undefined : durationMS/1000);
 	}
 }
 
+
+
+
+
+
+
 class UndoCapableDirector extends NonDecreasingDirector{
-	//todo. Make this able to backtrack
+    //thsi director uses both forwards and backwards arrows. the backwards arrow will undo any UndoCapableDirector.TransitionTo()s
+    //todo: hook up the arrows and make it not
 	constructor(options){
         super(options);
+
+        this.furthestSlideIndex = 0; //matches the number of times nextSlide() has been called
+        //this.currentSlideIndex is always < this.furthestSlideIndex - if equal, we release the promise and let nextSlide() return
+
+		this.undoStack = [];
+		this.undoStackIndex = 0; //increased by one every time either this.TransitionTo is called or this.nextSlide() is called
+
+		this.leftArrow = new DirectionArrow(false);
+		document.body.appendChild(this.leftArrow.arrowImage);
+		this.leftArrow.onclickCallback = function(){} //todo: put this in
+
+        this.nextSlideResolveFunction = function(){} //if you press right before the first director.nextSlide(), don't error
+
+        let self = this;
+        function keyListener(e){
+	    	if(e.repeat)return; //keydown fires multiple times but we only want the first one
+			let slideDelta = 0;
+			switch (e.keyCode) {
+			  case 34:
+			  case 39:
+			  case 40:
+				slideDelta = 1;
+				break;
+              case 33:
+              case 37:
+              case 38:
+                slideDelta = -1;
+			  default:
+				break;
+			}
+			if(slideDelta != 0){
+                if(slideDelta == 1){
+                    self.handleForwardsPress();
+                }else if(slideDelta == -1){
+                    self.handleBackwardsPress();
+                }
+			}
+		}
+
+		window.addEventListener("keydown", keyListener);
 	}
 
-	async nextSlide(){    if(!this.initialized)throw new Error("ERROR: Use .begin() on a Director before calling any other methods!");
+    moveFurtherIntoPresentation(){
+            //when not in the past (and therefore with nothing to redo), advance further.
+            //if there are less HTML slides than calls to director.newSlide(), complain in the console but allow the presentation to proceed
+            if(this.currentSlideIndex < this.numSlides){
+                this.undoStackIndex += 1; //advance past the NewSlideUndoItem
+                this.furthestSlideIndex += 1; 
+                this.currentSlideIndex += 1; //should still equal furthestSlideIndex
+            }
+
+            this.showSlide(this.currentSlideIndex); //this will complain in the console window if there are less slides than newSlide() calls
+            this.nextSlideResolveFunction(); //allow presentation code to proceed
+    }
+
+    handleForwardsPress(){
+        if(this.furthestSlideIndex == this.currentSlideIndex){
+            //unpause code execution and show next slide
+            this.moveFurtherIntoPresentation();
+            return;
+        }
+        // if we get to here, we've previously done an undo and we need to catch up
+
+        this.undoStackIndex += 1;
+        while(this.undoStack[this.undoStackIndex].constructor !== NewSlideUndoItem){
+            //loop through undo stack and redo each undo
+
+            if(this.undoStackIndex == this.undoStack.length){
+                //fully undone and at current slide
+                break;
+            }
+
+            //redo transformation in this.undoStack[this.undoStackIndex]
+            let redoItem = this.undoStack[this.undoStackIndex]
+            var redoAnimation = new Animation(redoItem.target, redoItem.toValues, redoItem.durationMS === undefined ? undefined : redoItem.durationMS/1000);
+            //and now redoAnimation, having been created, goes off and does its own thing I guess. this seems inefficient. todo: fix that and make them all centrally updated by the animation loop orsomething
+
+            this.undoStackIndex += 1;
+        }
+        this.currentSlideIndex += 1;
+        this.showSlide(this.currentSlideIndex);
+    }
+
+    handleBackwardsPress(){
+
+        if(this.undoStackIndex == 0 || this.currentSlideIndex == 0){
+            return;
+        }
+
+        this.undoStackIndex -= 1;
+        while(this.undoStack[this.undoStackIndex].constructor !== NewSlideUndoItem){
+            //loop through undo stack and redo each undo
+
+            if(this.undoStackIndex == 0){
+                //at first slide
+                break;
+            }
+
+            //undo transformation in this.undoStack[this.undoStackIndex]
+            let undoItem = this.undoStack[this.undoStackIndex];
+            let duration = undoItem.durationMS === undefined ? 1 : undoItem.durationMS/1000;
+            duration = Math.min(duration / 2, 1); //undoing should be faster, so cut it in half - but cap durations at 1s
+            var undoAnimation = new Animation(undoItem.target, undoItem.fromValues, duration);
+            //and now undoAnimation, having been created, goes off and does its own thing I guess. this seems inefficient. todo: fix that and make them all centrally updated by the animation loop orsomething
+
+            this.undoStackIndex -= 1;
+        }
+        this.currentSlideIndex -= 1;
+        this.showSlide(this.currentSlideIndex);
+
+    }
+
+	async nextSlide(){
+        /*The user will call this function to mark the transition between one slide and the next. This does two things:
+        A) waits until the user presses the right arrow key, returns, and continues execution until the next nextSlide() call
+        B) if the user presses the left arrow key, they can undo and go back in time, and every TransitionTo() call before that will be undone until it reaches a previous nextSlide() call. Any normal javascript assignments won't be caught in this :(
+        C) if undo
+        */
+        
+        this.numSlides++;
+        this.undoStack.push(new NewSlideUndoItem(this.currentSlideIndex));
+
+
+        if(!this.initialized)throw new Error("ERROR: Use .begin() on a Director before calling any other methods!");
 
 		let self = this;
 
-		this.rightArrow.showSelf();
-		//promise is resolved by calling this.nextSlidePromise.resolve() when the time comes
-
+		//promise is resolved by calling this.nextSlideResolveFunction() when the time comes
 		return new Promise(function(resolve, reject){
-			function keyListener(e){
-				if(e.repeat)return; //keydown fires multiple times but we only want the first one
-				let slideDelta = 0;
-				switch (e.keyCode) {
-				  case 34:
-				  case 39:
-				  case 40:
-					slideDelta = 1;
-					break;
-                  case 33:
-                  case 37:
-                  case 38:
-                    slideDelta = -1;
-				  default:
-					break;
-				}
-				if(slideDelta != 0){
-					self._changeSlide(slideDelta, resolve);
-					self.rightArrow.hideSelf();
-					window.removeEventListener("keydown",keyListener); //this approach taken from https://stackoverflow.com/questions/35718645/resolving-a-promise-with-eventlistener
-				}
-			}
-
-			window.addEventListener("keydown", keyListener);
-			//horrible hack so that the 'next slide' arrow can trigger this too
 			self.nextSlideResolveFunction = function(){ 
 				resolve();
-				window.removeEventListener("keydown",keyListener); 
 			}
 		});
+
 	}
-	transitionTo(target, toValues, durationMS){
-		//EXP.Utils.Assert(this.undoStackIndex == 0); //This may not work well.
-		var animation = new EXP.Animation(target, toValues, durationMS === undefined ? undefined : durationMS/1000);
+	TransitionTo(target, toValues, durationMS){
+		var animation = new Animation(target, toValues, durationMS === undefined ? undefined : durationMS/1000);
 		let fromValues = animation.fromValues;
-		this.undoStack.push(new EXP.Director.UndoItem(target, toValues, fromValues, durationMS));
+		this.undoStack.push(new UndoItem(target, toValues, fromValues, durationMS));
 		this.undoStackIndex++;
 	}
 }
 
-EXP.Director.UndoItem = class UndoItem{
+
+
+class UndoItem{
 	constructor(target, toValues, fromValues, durationMS){
 		this.target = target;
 		this.toValues = toValues;
@@ -239,7 +343,7 @@ EXP.Director.UndoItem = class UndoItem{
 		this.durationMS = durationMS;
 	}
 }
-EXP.Director.NewSlideUndoPoint = class NewSlide{
+class NewSlideUndoItem{
 	constructor(slideIndex){
         this.slideIndex = slideIndex;
 	}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-export { Array } from './js/Array.js';
-export { Area } from './js/Area.js';
+export * from './js/Array.js';
+export * from './js/Area.js';
 
 export { Transformation } from './js/Transformation.js';
-export { HistoryRecorder } from './js/HistoryRecorder.js';
+export * from './js/HistoryRecorder.js';
 
-export { TransitionTo, Animation } from './js/Animation.js';
+export * from './js/Animation.js';
 
 export { setupThree, ThreeasyEnvironment, ThreeasyRecorder }from './js/three_bootstrap.js';
 export { threeEnvironment } from './js/ThreeEnvironment.js';
@@ -13,9 +13,9 @@ export { Utils } from './js/utils.js';
 export { Math } from './js/math.js'; // may need to be changed depending on how it should be done. congruent_utilities.js definitely needs to change if so;
 export * from './js/asyncDelayFunctions.js';
 //outputs
-export { LineOutput } from './js/outputs/LineOutput.js';
-export { PointOutput } from './js/outputs/PointOutput.js';
-export { VectorOutput } from './js/outputs/VectorOutput.js';
-export { SurfaceOutput } from './js/outputs/SurfaceOutput.js';
+export * from './js/outputs/LineOutput.js';
+export * from './js/outputs/PointOutput.js';
+export * from './js/outputs/VectorOutput.js';
+export * from './js/outputs/SurfaceOutput.js';
 
-export { NonDecreasingDirector, DirectionArrow } from './js/Director.js';
+export * from './js/Director.js';

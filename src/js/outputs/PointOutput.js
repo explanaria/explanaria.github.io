d7df9642d60d5f2f5598928b76d816e400ffdddb
Explanaria <explanaria@gmail.com>
2019-05-18 00:09:24 -0400

    Make each PointOutput use its own material

--- src/js/outputs/PointOutput.js
+++ src/js/outputs/PointOutput.js
@@ -1,5 +1,5 @@
-import Point from './Point.js';
 import {OutputNode} from '../Node.js';
+import { threeEnvironment } from '../ThreeEnvironment.js';
 
 class PointOutput extends OutputNode{
 	constructor(options = {}){
@@ -14,8 +14,10 @@ class PointOutput extends OutputNode{
 		this._color = options.color !== undefined ? options.color : 0x55aa55;
 		this._opacity = options.opacity !== undefined ? options.opacity : 1;
 
-
 		this.points = [];
+
+        this.material = new THREE.MeshBasicMaterial({color: this._color});
+        this.opacity = this._opacity; //trigger setter to set this.material's opacity properly
 
 		this.numCallsPerActivation = 0; //should always be equal to this.points.length
 		this._activatedOnce = false;
@@ -28,7 +30,7 @@ class PointOutput extends OutputNode{
 
 		if(this.points.length < this.numCallsPerActivation){
 			for(var i=this.points.length;i<this.numCallsPerActivation;i++){
-				this.points.push(new Point({width: 1,color:this._color, opacity:this._opacity}));
+				this.points.push(new PointMesh({width: 1,material:this.material}));
 				this.points[i].mesh.scale.setScalar(this._width); //set width by scaling point
 			}
 		}
@@ -56,22 +58,19 @@ class PointOutput extends OutputNode{
 		}
     }
 	set opacity(opacity){
-		//technically this will set all points of the same color, and it'll be wiped with a color change. But I'll deal with that sometime later.
-		for(var i=0;i<this.numCallsPerActivation;i++){
-			let mat = this.getPoint(i).mesh.material;
-			mat.opacity = opacity; //instantiate the point
-			mat.transparent = opacity < 1;
-            mat.visible = opacity > 0;
-		}
+		//technically this sets all points to the same color. Todo: allow different points to be differently colored.
+		
+		let mat = this.material;
+		mat.opacity = opacity; //instantiate the point
+		mat.transparent = opacity < 1;
+        mat.visible = opacity > 0;
 		this._opacity = opacity;
 	}
 	get opacity(){
 		return this._opacity;
 	}
 	set color(color){
-		for(var i=0;i<this.points.length;i++){
-			this.getPoint(i).color = color;
-		}
+        this.material.color = color;
 		this._color = color;
 	}
 	get color(){
@@ -91,6 +90,52 @@ class PointOutput extends OutputNode{
 	}
 }
 
+
+class PointMesh{
+	constructor(options){
+		/*options:
+			x,y: numbers
+			width: number
+            material: 
+		*/
+
+		let width = options.width === undefined ? 1 : options.width
+        this.material = options.material; //one material per PointOutput
+
+		this.mesh = new THREE.Mesh(this.sharedCircleGeometry,this.material);
+
+		this.mesh.position.set(this.x,this.y,this.z);
+		this.mesh.scale.setScalar(this.width/2);
+		threeEnvironment.scene.add(this.mesh);
+
+		this.x = options.x || 0;
+		this.y = options.y || 0;
+		this.z = options.z || 0;
+	}
+	removeSelfFromScene(){
+		threeEnvironment.scene.remove(this.mesh);
+	}
+	set x(i){
+		this.mesh.position.x = i;
+	}
+	set y(i){
+		this.mesh.position.y = i;
+	}
+	set z(i){
+		this.mesh.position.z = i;
+	}
+	get x(){
+		return this.mesh.position.x;
+	}
+	get y(){
+		return this.mesh.position.y;
+	}
+	get z(){
+		return this.mesh.position.z;
+	}
+}
+PointMesh.prototype.sharedCircleGeometry = new THREE.SphereGeometry(1/2, 8, 6); //radius 1/2 makes diameter 1, so that scaling by n means width=n
+
 //testing code
 function testPoint(){
 	var x = new EXP.Area({bounds: [[-10,10]]});
@@ -101,4 +146,4 @@ function testPoint(){
 	x.activate();
 }
 
-export {PointOutput}
+export {PointOutput, PointMesh}

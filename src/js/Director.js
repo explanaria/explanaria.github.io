907724cf26bc78adbe5c437f5188fa94fe3c6fee
Explanaria <explanaria@gmail.com>
2021-08-21 00:35:55 -0400

    Eliminate lag spike upon clicking next

--- src/js/Director.js
+++ src/js/Director.js
@@ -233,22 +233,7 @@ class NonDecreasingDirector{
 
     }
     scrollUpToTopOfContainer(element){
-        this.getScrollParent(element).scrollTop = 0;
-    }
-    getScrollParent(element, includeHidden){
-        //from https://stackoverflow.com/questions/35939886/find-first-scrollable-parent
-        var style = getComputedStyle(element);
-        var excludeStaticParent = style.position === "absolute";
-        var overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
-        if (style.position === "fixed") return document.body;
-        for (var parent = element; (parent = parent.parentElement);) {
-            style = getComputedStyle(parent);
-            if (excludeStaticParent && style.position === "static") {
-                continue;
-            }
-            if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX)) return parent;
-        }
-        return document.body;
+        element.scrollIntoView(true);
     }
 
     //verbs

a971858eae216b6a3e0d8b55174b1c9d50948aec
Explanaria <explanaria@gmail.com>
2021-08-21 16:42:01 -0400

    Update rollup to build to the right place

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,7 @@ export default [{
 {
   input: 'src/main.js',
   output: {
-    file: 'site/resources/build/explanaria-bundle.js',
+    file: 'docs/resources/build/explanaria-bundle.js',
     format: 'umd',
 	format_i_wish_i_could_use: 'es',
     name: 'EXP',

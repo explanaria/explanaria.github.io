4128da51a2fe07105a189c56630348bde7abf2b2
Explanaria <explanaria@gmail.com>
2021-08-24 12:01:51 -0400

    Move slightly slower in algebra

--- docs/impossibletriangles/2-squareproblem.js
+++ docs/impossibletriangles/2-squareproblem.js
@@ -296,6 +296,7 @@ async function animate(){
     await presentation.nextSlide();
     await presentation.nextSlide();
     await presentation.nextSlide();
+    await presentation.nextSlide();
 }
 
 

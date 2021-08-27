2f07f9641ff2a9d83644abe45b9aeba1608dbeeb
Explanaria <explanaria@gmail.com>
2021-08-27 00:01:18 -0400

    Integrate goat evaluation

--- docs/impossibletriangles/2.js
+++ docs/impossibletriangles/2.js
@@ -208,9 +208,26 @@ function setup(){
     staticObjects.forEach(i => i.activate(0));
 }
 
+function setupGoat(presentation){
+    //analytics
+    document.addEventListener('visibilitychange', function(e) {
+        if (window.goatcounter === undefined)return;
+        if (document.visibilityState !== 'hidden')
+            return
+        if (goatcounter.filter())
+            return
+        navigator.sendBeacon(goatcounter.url({
+            event: true,
+            title: location.pathname + location.search + " unloaded on slide " + presentation.currentSlideIndex,
+            path: function(p) { return 'unload-' + p + '-slide-'+presentation.currentSlideIndex },
+        }))
+    })
+}
+
 async function animate(){
     let presentation = new EXP.UndoCapableDirector();
     await presentation.begin();
+    setupGoat(presentation);
 
 
     [aText, bText, cText, firstNText1, firstNText2].forEach(label => presentation.TransitionTo(label, {'opacity':1}));

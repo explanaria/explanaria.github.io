2c8dc235ee6b018ea74c7f3c41791c908f965410
Explanaria <explanaria@gmail.com>
2022-03-21 18:34:08 +0100

    Make intro reactive and add popup, to be worked on

--- docs/sketches/twoniverse/src/Intro.svelte
+++ docs/sketches/twoniverse/src/Intro.svelte
@@ -1,4 +1,8 @@
 <script>
+    export let startEquation = [2,3,8];//2*3 = 8
+    $: a = startEquation[0]
+    $: b = startEquation[1]
+    $: AUProduct = startEquation[2]
 </script>
 
 <style>
@@ -8,19 +12,17 @@
     }
 </style>
 
-<div>
-    <h1>Spider-Math: Into the 2*3=8iverse</h1>
+<h1>Spider-Math: Into the {a}*{b}={AUProduct}iverse</h1>
 
-    <p>Welcome to the 2*3=8niverse.</p>
+<p>Welcome to the {a}*{b}={AUProduct}niverse.</p>
 
-    <p>In our universe, multiplication only works in one way. 2*3 is always 6. But why 6 in particular? It's easy to write down other equations like 2+2=5 - they're just not true in the universe we normally work with.
-    </p>
+<p>In our universe, multiplication only works in one way. {a}*{b} is always {a*b}. But why {a*b} in particular? It's easy to write down other equations like 2+2=5 - they're just not true in the universe we normally work with.
+</p>
 
-    <p>...but what about other universes? What if there was an 🌌alternate universe🌌 which followed all the same math rules, but where 2*3 was, say, 8 instead of 6? Will everything implode into a puff of contradictions? Or will it somehow stay internally consistent? Let's find out.</p>
+<p>...but what about other universes? What if there was an 🌌alternate universe🌌 which followed all the same math rules, but where {a}*{b} was, say, {AUProduct} instead of {a*b}? Will everything implode into a puff of contradictions? Or will it somehow stay internally consistent? Let's find out.</p>
 
-    <p>Below is an <b>alternate-universe multiplication table</b>, from an alternate universe where the rules of addition and multiplication work the same but 2*3 is 8 instead of 6. </p>
+<p>Below is an <b>alternate-universe multiplication table</b>, from an alternate universe where the rules of addition and multiplication work the same but {a}*{b} is {AUProduct} instead of {a*b}. </p>
 
-    <p>In our universe, if you move sideways along a row of a multiplication table, you add the same number repeatedly. That's true because of the <u>distributive property</u> in our universe - so if the rules of this alternate universe are the same, we can still use the distributive property to move sideways across rows and up/down across columns.</p>
+<p>In our universe, if you move sideways along a row of a multiplication table, you add the same number repeatedly. That's true because of the <u>distributive property</u> in our universe - so if the rules of this alternate universe are the same, we can still use the distributive property to move sideways across rows and up/down across columns.</p>
 
-    <p>Click the arrows to fill in the multiplication table, and you might find some interesting alternate-universe equations along the way.</p>
-</div>
+<p>Click the arrows to fill in the multiplication table, and you might find some interesting alternate-universe equations along the way.</p>

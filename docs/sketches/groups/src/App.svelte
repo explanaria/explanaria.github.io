041182a7df2cce21f2680012fcb9849141a7a682
Explanaria <explanaria@gmail.com>
2022-07-22 22:29:20 -0400

    Show bonds between molecules

--- docs/sketches/groups/src/App.svelte
+++ docs/sketches/groups/src/App.svelte
@@ -4,14 +4,19 @@
 </script>
 
 <style>
-    .main{
+    .maincontainer{
         font-family: Lora,serif;
+        aspect-ratio: 16 / 9;
+        max-width: 100vw;
+        max-height: 100vh;
+        margin: auto;
+    }
+    body{
+        margin: 0;
     }
 </style>
 
-
-<MoleculeCanvas />
-
-<div class="main">
+<div class="maincontainer">
+    <MoleculeCanvas />
     <InteractiveD6Creator />
 </div>

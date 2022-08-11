7fa0ca41a032b1f3d050402ec08281a6271154be
Explanaria <explanaria@gmail.com>
2022-08-11 16:56:28 -0700

    Add a second chapter

--- docs/sketches/groups/src/App.svelte
+++ docs/sketches/groups/src/App.svelte
@@ -1,22 +1,25 @@
 <script>
-	import InteractiveD6Creator from "./InteractiveD6Creator.svelte";
-	import CayleyGraphIntroPart from "./CayleyGraphIntroPart.svelte";
-    import CrystalIntroPart from "./CrystalIntroPart.svelte";
+	import CayleyGraphIntroPart from "./chapters/CayleyGraphIntroPart.svelte";
+    import CrystalIntroPart from "./chapters/CrystalIntroPart.svelte";
+    import HolesInCrystalPart from "./chapters/HolesInCrystalPart.svelte";
 
-    let mode = 1;
-    let numModes = 2;
+    let chapter = 1; //zero indexed
+    let numChapters = 3;
     function swapmode(){
-        mode = (mode + 1) % numModes;
+        chapter = (chapter + 1) % numChapters;
     }
 </script>
 
 <div class="maincontainer">
     <div class="rotatesign"> <!-- todo: put a message here --> </div>
-    <span style:position="absolute" style:bottom="2em" style:left="0em">Chapter {mode}. <button on:click={swapmode} >Swap chapter</button></span>
-    {#if mode == 0}
+    <span style:position="absolute" style:bottom="2em" style:left="0em">Chapter {chapter}. <button on:click={swapmode} >Swap chapter</button></span>
+    {#if chapter == 0}
         <CrystalIntroPart />
     {/if}
-    {#if mode == 1}
+    {#if chapter == 1}
+        <HolesInCrystalPart />
+    {/if}
+    {#if chapter == 2}
         <CayleyGraphIntroPart />
     {/if}
 </div>
